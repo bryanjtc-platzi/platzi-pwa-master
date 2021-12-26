650d0d6a535b268e9ff46504c3e31853d59c9f84
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2021-12-25 19:14:35 -0500

    Replace react-scripts Fixes #21, Fixes #11

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Recipe from "./pages/Recipe";
+import Timer from "./pages/Timer";
+import IsOffline from "./components/IsOffline";
+import "./App.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">
+            Recetas <IsOffline>Offline</IsOffline>
+          </Link>
+          <Link to="/timer" className="timerLink">
+            ⏱
+          </Link>
+        </header>
+        <main>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/recipe/:recipeId" element={<Recipe />} />
+            <Route path="/timer" element={<Timer />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
